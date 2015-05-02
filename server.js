658e62eee02ed19a2e7e0b9d0c3de7e6f36bb912
Kevin Lanni <therealklanni@gmail.com>
2015-05-01 23:24:05 -0700

    add some routes

--- server.js
+++ server.js
@@ -7,8 +7,25 @@ var hbs = require('express-handlebars');
 var session = require('express-session');
 
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var validate = require('./lib/validate');
 var rateLimit = require('./lib/rate-limit');
+
+var dotty = require('dotty');
+var fs = require('fs');
+var path = require('path');
+var yaml = require('js-yaml');
+var assign = require('lodash/object/assign');
+
+var strings = yaml.safeLoad(fs.readFileSync(path.resolve('./strings.yml')));
+
+strings.signin.clientId = process.env.GOOGLE_CLIENTID;
+var nav = {
+  title: strings.title,
+  user: {}
+}
+strings.signin.title = strings.title;
+strings.apply.title = strings.title;
 
 app.engine('.hbs', hbs({
   defaultLayout: 'main',
@@ -25,16 +42,55 @@ app.use(
     secret: process.env.SESSION_SECRET || 'hack me pls, kthx'
   }),
   cookieParser(),
-  validate()
+  bodyParser.urlencoded({ extended: true }),
+  bodyParser.json(),
+  function (req, res, next) {
+    var user = dotty.get(req, 'session.user');
+
+    if (user) {
+      res.locals.displayName = user.displayName;
+    }
+
+    next();
+  }
 );
 
-app.post('/apply', rateLimit());
-
 app.get('/', function (req, res) {
-  res.render('main', {
-    title: 'Slack Invite Request'
-  });
+  res.render('main', assign({}, strings, {
+    user: res.locals.displayName
+  }));
 });
+
+app.get('/signin', function (req, res) {
+  var user = dotty.get(req, 'session.user');
+  console.log('locals', res.locals)
+
+  if (user) {
+    res.redirect('/apply');
+  } else {
+    res.render('signin', strings.signin);
+  }
+});
+
+app.post('/signin', rateLimit(), function (req, res) {
+  var user = dotty.get(req, 'body.user');
+
+  if (user && user.kind === 'plus#person') {
+    console.log('User "%s" logged in', user.displayName);
+    req.session.user = user;
+    res.sendStatus(200).end();
+  } else {
+    res.sendStatus(401).end();
+  }
+});
+
+app.get('/apply', validate(), function (req, res) {
+  res.render('apply', assign({}, strings.apply, {
+    user: res.locals.displayName
+  }));
+});
+
+app.post('/apply', rateLimit());
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
